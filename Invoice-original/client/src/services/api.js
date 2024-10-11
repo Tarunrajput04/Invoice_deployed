1cec9bd29a2788634424173e6ea1f55f0a93b44b
Tarun Kumar <89685711+Tarunrajput04@users.noreply.github.com>
2024-10-11 09:36:20 +0000

    apiupdated

--- Invoice-original/client/src/services/api.js
+++ Invoice-original/client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://brilliant-exploration-production.up.railway.app';
 
 export const getAllInvoices = async () => {
     try {
